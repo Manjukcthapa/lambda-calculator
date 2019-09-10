75355074110598736b7f350fb09e17291e9425bd
Manju KC <kcmanjuthapa@gmail.com>
2019-09-10 12:47:43 -0400

    added export in data.js

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 //import any components needed
+import React, {useState} from "react";
+import NumberButton from './NumberButton';
 // example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
+ 
+
 import { numbers } from '../../../data' 
-*/
+
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+const Numbers = (props) => {
+  const [calNumbers, setNumber] = useState(numbers);
   // STEP 2 - add the imported data to state
   return (
     <div>
