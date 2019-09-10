4da090ced0211eb6832aae2b7e120aadbd9d714c
Manju KC <kcmanjuthapa@gmail.com>
2019-09-10 12:59:38 -0400

    imported NumberButton and numbers to the Number.js

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -3,15 +3,13 @@ import React, {useState}from "react";
 //import any components needed
 import React, {useState} from "react";
 import NumberButton from './NumberButton';
+import {numbers} from "/Users/red/Desktop/lambda/projects/lambda-calculator/src/data"
+
 // example of import from data.js. Note all the ../   This is how we move through folders. 
  
-
-import { numbers } from '../../../data' 
-
 //Import your array data to from the provided data file
 
 const Numbers = (props) => {
-  const [calNumbers, setNumber] = useState(numbers);
   // STEP 2 - add the imported data to state
   return (
     <div>
