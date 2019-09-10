0142a7b40f0021056f168adc5b2cfa063f602761
Manju KC <kcmanjuthapa@gmail.com>
2019-09-10 13:02:32 -0400

    added the imported data to state

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -9,8 +9,9 @@ import {numbers} from "/Users/red/Desktop/lambda/projects/lambda-calculator/src/
  
 //Import your array data to from the provided data file
 
-const Numbers = (props) => {
+const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = useState(numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
