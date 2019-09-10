7de550267e0647ab065d590ad9212920172049e0
Manju KC <kcmanjuthapa@gmail.com>
2019-09-10 13:29:10 -0400

    added button to the operatorButton.js

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,14 +1,11 @@
 import React from "react";
 
-const NumberButton = ({number}) => {
+const NumberButton = ({ number }) => {
   return (
     <>
-     
-    
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       <button> {number} </button>
     </>
-
   );
 };
 
