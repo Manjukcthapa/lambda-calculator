40fe00a94f5fcefad98b21204b72c137a933ec46
Manju KC <kcmanjuthapa@gmail.com>
2019-09-10 13:25:31 -0400

    imported number component  to the app js

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,9 +1,8 @@
 import React, {useState}from "react";
 
 //import any components needed
-import React, {useState} from "react";
 import NumberButton from './NumberButton';
-import {numbers} from "/Users/red/Desktop/lambda/projects/lambda-calculator/src/data"
+import {numbers} from "../../../data.js"
 
 // example of import from data.js. Note all the ../   This is how we move through folders. 
  
@@ -17,6 +16,7 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+        console.log(numberState);
       <div > {numbers.map((item,i) => <NumberButton key={i}number={item}/>)}</div>
     </div>
   );
